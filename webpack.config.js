acffc918358df5ca23e7a44af286983fe95f1203
TrakaDev <trakadev@gamil.com>
2022-04-03 19:52:12 -0500

    sass integrated

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -28,13 +29,33 @@ module.exports = {
                 use: {
                     loader: 'html-loader'
                 }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
             }
         ]
     },
-    plugins: [new HtmlWebpackPlugin(
+    plugins: [
+        new HtmlWebpackPlugin(
             {
                 template: './public/index.html',
                 filename: './index.html'
             }
-        )]
+        ),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 7777,
+    }
 };
