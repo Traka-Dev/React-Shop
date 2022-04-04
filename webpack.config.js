ee359bf326dd715e7c2b6bc9df9b4a27f3b62a0b
TrakaDev <trakadev@gamil.com>
2022-04-03 23:28:50 -0500

    Recovery Password

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/',
     },
     mode: 'development',
     resolve: {
@@ -66,6 +67,7 @@ module.exports = {
         }),
     ],
     devServer: {
+        historyApiFallback: true,
         static: {
             directory: path.join(__dirname, 'public'),
         },
