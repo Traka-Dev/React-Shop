e55d2eed47fed058c1d417a268763eb30480856b
TrakaDev <trakadev@gamil.com>
2022-04-06 01:05:19 -0500

    webpack Alias and Assets

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../styles/ProductItem.scss';
+import '@styles/ProductItem.scss';
+import add_to_cart_icon from '@icons/bt_add_to_cart.svg'
 
 const ProductItem = () => {
 	return (
@@ -11,7 +12,7 @@ const ProductItem = () => {
 					<p>Bike</p>
 				</div>
 				<figure>
-					<img src="./icons/bt_add_to_cart.svg" alt="" />
+					<img src={add_to_cart_icon} alt="" />
 				</figure>
 			</div>
 		</div>
